cc47870e1651cc2ee1b431d071e05bfe4414d3fb
Rafael <rafaelgoncalvesbarreira@gmail.com>
2021-10-25 11:41:24 -0300

    changin from complex OOP to simple funcional

--- plotviewer/plotviewer.py
+++ plotviewer/plotviewer.py
@@ -7,27 +7,24 @@ from matplotlib.backends.backend_tkagg import (
 from matplotlib.backend_bases import key_press_handler
 from matplotlib.figure import Figure
 
-class PlotViewer():
-  def __init__(self, figure, width=800, heigtht=500):
-    self.root = tk.Tk()
-    self.root.minsize(width, heigtht)
-    self.root.wm_title("Plot Viewer")
-    self.create_widgets(figure)
+root = tk.Tk()
+
+def show(figure, width=800, heigtht=500):
+  root.minsize(width, heigtht)
+  root.wm_title("Plot Viewer")
+
+  canvas = FigureCanvasTkAgg(figure, master=root)  # A tk.DrawingArea.
+  canvas.draw()
+  canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+  toolbar = NavigationToolbar2Tk(canvas, root)
+  toolbar.update()
+
+  button = tk.Button(master=root, text="Close", command=_quit)
+  button.pack(side=tk.BOTTOM)
   
-  def create_widgets(self, figure):
-    canvas = FigureCanvasTkAgg(figure, master=self.root)  # A tk.DrawingArea.
-    canvas.draw()
-    canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+  root.mainloop()
 
-    toolbar = NavigationToolbar2Tk(canvas, self.root)
-    toolbar.update()
-
-    button = tk.Button(master=self.root, text="Close", command=self._quit)
-    button.pack(side=tk.BOTTOM)
-  
-  def show(self):
-    self.root.mainloop()
-
-  def _quit(self):
-    self.root.quit()     # stops mainloop
-    self.root.destroy() 
+def _quit():
+    root.quit()  
+    root.destroy()
--- tests/test_render.py
+++ tests/test_render.py
@@ -1,16 +1,7 @@
-import sys
-from matplotlib import figure
-sys.path.append("..")
-import numpy as np
 from matplotlib.figure import Figure
-from plotviewer import PlotViewer
+import plotviewer.plotviewer as pv
 
-fig = Figure(figsize=(5, 4), dpi=100)
-t = np.arange(0, 3, .01)
-fig.add_subplot(111).plot(t, 2 * np.sin(2 * np.pi * t))
+fig = Figure()
+fig.add_subplot(111).plot([5,1,9,2,4,8,3])
 
-fig2 = Figure()
-fig2.add_subplot(111).plot([5,1,9,2,4,8,3])
-
-view = PlotViewer(fig2)
-view.show()
+pv.show(fig)
