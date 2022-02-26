3c659307032b0431638d274910b6031453194352
Rafael <rafaelgoncalvesbarreira@gmail.com>
2022-02-26 00:59:00 -0300

    new example

--- tests/test_render.py
+++ tests/test_render.py
@@ -2,6 +2,9 @@ from matplotlib.figure import Figure
 import plotviewer.plotviewer as pv
 
 fig = Figure()
-fig.add_subplot(111).plot([5,1,9,2,4,8,3])
+ax = fig.add_subplot(111)
+ax.plot([5,1,9,2,4,8,3])
+pv.show(fig)
 
+ax.plot([1,9,2,4,8,3,5])
 pv.show(fig)
